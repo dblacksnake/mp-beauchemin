e0979b4835f5a6a450b6cf0df43fb2a149e171d5
dblacksnake <63682911+dblacksnake@users.noreply.github.com>
2023-05-24 22:37:32 -0400

    vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,10 @@
-import { createApp } from 'vue'
 import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
-  state :{
-    baba:'baba'
-  },
+  state: {
+    baba: 'baba'
+  }
 })
 
-const app = createApp({ /* your root component */ })
-
-// Install the store instance as a plugin
-app.use(store)
+export default store
